488b1ced756a7c432be87729f721b20b685b58f8
Cloud User <ec2-user@ip-10-0-16-178.us-east-2.compute.internal>
2023-06-29 18:27:14 +0000

    this is a maven project using the src to create an artifact

---
+++ checking-app/src/main/java/primusLearning/App.java
@@ -0,0 +1,13 @@
+package primusLearning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
